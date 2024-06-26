93d824ab8efa533688baccb28474ab508f754444
ZXl45sa4 <120339826+ZXl45sa4@users.noreply.github.com>
2024-06-26 12:23:08 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,7 @@
+my_string=str(input())                          #строки с произвольным текстом
+print(len(my_string))                           #количество символов введённого текста
+print(my_string.upper())                        #верхний регистр
+print(my_string. lower())                       #нижний регистр
+print(my_string.replace(' ',''))    #удаление пробелов
+print(my_string[0])                             #первый символ в строки
+print(my_string[-1])                            #последний символ строки
