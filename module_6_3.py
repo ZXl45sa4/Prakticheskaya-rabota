c190861b798983af1bf66eb42e8bca3abb0e9568
ZXl45sa4 <120339826+ZXl45sa4@users.noreply.github.com>
2024-08-22 18:03:20 +0300

    Add files via upload

--- module_6_3.py
+++ module_6_3.py
@@ -1,19 +1,24 @@
 class Horse: #класс описывающий лошадь
-    sound = 'Frrr'  # звук, который издаёт лошадь
+    def __init__(self):
+        self.x_distance = 0  # пройденный путь
+        self.sound = 'Frrr'  # звук, который издаёт лошадь
+
     def run(self, dx):  #где dx - изменение дистанции
         self.x_distance += dx   #увеличивает x_distance на dx
-    x_distance = 0  #пройденный путь
-
 
 class Eagle:    # класс описывающий орла
-    def voice (self):
+    def __init__(self):
+        self.y_distance = 0  # высота полёта
         self.sound = 'I train, eat, sleep, and repeat'  # звук, который издаёт орёл (отсылка)
+
     def fly(self, dy):  #где dy - изменение дистанции
         self.y_distance += dy   #увеличивает y_distance на dy
-    y_distance = 0  #высота полёта
-
 
 class Pegasus (Horse, Eagle):   #класс описывающий пегаса
+    def __init__(self):
+        Horse.__init__(self)
+        Eagle.__init__(self)
+
     def move(self, dx: int, dy: int):   #где dx и dy изменения дистанции/
         self.run(dx)    #запускаться наследованные методы run
         self.fly(dy)    ##запускаться наследованные методы fly
@@ -22,7 +27,6 @@ class Pegasus (Horse, Eagle):   #класс описывающий пегаса
         return (self.x_distance, self.y_distance)
 
     def voice(self):    #который печатает значение унаследованного атрибута sound
-        super().voice()
         print(f'{self.sound}')
         return
 
