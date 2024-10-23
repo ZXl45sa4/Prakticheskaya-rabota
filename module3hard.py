a645da3751aa66747651c59477a80609e7dc8fc1
ZXl45sa4 <120339826+ZXl45sa4@users.noreply.github.com>
2024-10-23 22:31:57 +0300

    Add files via upload

--- module3hard.py
+++ module3hard.py
@@ -1,16 +1,26 @@
+def example_function(*args):
+    summa = 0
+    for cycle in args:
+        if isinstance(cycle, dict):
+            for key, value in cycle.items():
+                summa += example_function(key)
+                summa += example_function(value)
+        elif isinstance(cycle, list | tuple | set):
+            for i in cycle:
+                summa += example_function(i)
+        elif isinstance(cycle, int | float):
+            summa += cycle
+        elif isinstance(cycle, str):
+            summa += len(cycle)
+    return summa
+
+
 data_structure = [
-  [1, 2, 3],
-  {'a': 4, 'b': 5},
-  (6, {'cube': 7, 'drum': 8}),
-  "Hello",
-  ((), [{(2, 'Urban', ('Urban2', 35))}])
+    [1, 2, 3],
+    {'a': 4, 'b': 5},
+    (6, {'cube': 7, 'drum': 8}),
+    "Hello",
+    ((), [{(2, 'Urban', ('Urban2', 35))}])
 ]
-print(len(data_structure))
-print(list(data_structure))
-sum1 = sum(data_structure[0])
-print(sum1)
-sum2 = sum(data_structure[1].values())
-print(sum2)
-print(list(data_structure[2]))
-# result = calculate_structure_sum(data_structure)
-# print(result)
+
+print(example_function(*data_structure))
